9a26d0d6a59d416618bd7562aa9e33027cabc4c8
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-06-07 15:45:57 +0200

    carreplymanager & init live scan

--- DPIPolice/src/gateway/LivePoliceGateway.java
+++ DPIPolice/src/gateway/LivePoliceGateway.java
@@ -9,8 +9,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import livepolice.models.LivePoliceReply;
 import livepolice.models.LivePoliceRequest;
+import message.CarReplyManager;
 import message.MessageReceiver;
 import message.MessageSender;
 import observer.Observable;
@@ -21,36 +23,87 @@ import observer.Observer;
  * @author Tomt
  */
 public class LivePoliceGateway implements Observer, Observable {
+
     private List<Observer> observers = new ArrayList<>();
     private MessageSender sender;
     private MessageReceiver receiver;
+    private MessageSender carSender;
+    private MessageReceiver carReceiver;
+
     private ISerializer serializer;
     private Map<LivePoliceRequest, String> correlations = new HashMap<>();
+    private Map<String, CarReplyManager> resultsForCorrelationID = new HashMap<>();
+    private int numberOfCars = 5;
 
     public LivePoliceGateway(String senderTopic, String receiverTopic) {
         sender = new MessageSender("destination", senderTopic);
         receiver = new MessageReceiver("destination", receiverTopic);
+
+        carSender = new MessageSender("destination", "car");
+        carReceiver = new MessageReceiver("destination", "car");
+
         serializer = new LivePoliceSerializer();
-        
+
         receiver.addObserver(this);
     }
-    
-    public void receiveRequest(String content, String correlationId){
+
+    public void receiveRequest(String content, String correlationId) {
         LivePoliceRequest request = (LivePoliceRequest) serializer.StringToRequest(content);
         correlations.put(request, correlationId);
         notifyObservers(request);
-        sendReply(new LivePoliceReply(true, "smth", request.getLicencePlate()), correlationId);
-        //start scan
+        //sendReply(new LivePoliceReply(true, "smth", request.getLicencePlate()), correlationId);
+        initLiveScan(correlationId);
     }
-    
-    public void sendReply(LivePoliceReply reply, String correlationId){
+
+    public void sendReply(LivePoliceReply reply, String correlationId) {
         sender.sendMessage(serializer.ReplyToString(reply), correlationId);
     }
-    
+
+    public void initLiveScan(String correlationId) {
+        String licencePlate = "";
+        for (int i = 0; i < numberOfCars; i++) {
+            licencePlate = Integer.toString(i);
+            sendCar("Car;;" + licencePlate, correlationId);
+        }
+        resultsForCorrelationID.put(correlationId, new CarReplyManager(numberOfCars));
+    }
+
+    public void sendCar(String licencePlate, String correlationId) {
+        carSender.sendMessage(licencePlate, correlationId);
+    }
+
+    public void receiveCar(String content, String correlationId) {
+        CarReplyManager carReplyManager = resultsForCorrelationID.get(correlationId);
+        for (Entry<LivePoliceRequest, String> entry : correlations.entrySet()) {
+            if (entry.getValue() == null ? correlationId == null : entry.getValue().equals(correlationId)) {
+                if (entry.getKey().getLicencePlate().equals(getCarLicencePlate(content))) {
+                    carReplyManager.newReply(new LivePoliceReply(true, "Maaskantje", "LocalMaaskantjeA"));
+                    sendReply(carReplyManager.getBestReply(), correlationId);
+                } else {
+                    carReplyManager.newReply(new LivePoliceReply(false, "None", "None"));
+                }
+            }
+        }
+
+        if (carReplyManager.isCompleted()) {
+            sendReply(new LivePoliceReply(false, "None", "None"), correlationId);
+        }
+    }
+
+    public String getCarLicencePlate(String content) {
+        String[] strings = content.split(";;");
+        System.out.println(strings[1]);
+        return strings[1];
+    }
+
     @Override
     public void update(Object... args) {
         String[] result = new String[]{args[0].toString(), args[1].toString()};
-        receiveRequest(result[0], result[1]);
+        if (result[0].startsWith("Car")) {
+            receiveCar(result[0], result[1]);
+        } else {
+            receiveRequest(result[0], result[1]);
+        }
     }
 
     @Override
@@ -65,9 +118,9 @@ public class LivePoliceGateway implements Observer, Observable {
 
     @Override
     public void notifyObservers(Object... args) {
-        for(Observer o : observers){
+        for (Observer o : observers) {
             o.update(args);
         }
     }
-    
+
 }
